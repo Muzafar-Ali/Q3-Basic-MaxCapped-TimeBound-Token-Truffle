93a70af72f663625e0791801cf65a79166ecca73
Muzafar-Ali <84478313+Muzafar-Ali@users.noreply.github.com>
2022-07-14 15:55:02 +0400

    Update CappedAndTimeBoundTokens.sol

--- contracts/CappedAndTimeBoundTokens.sol
+++ contracts/CappedAndTimeBoundTokens.sol
@@ -88,7 +88,7 @@ contract EducationToken is IERC20{
     }
 }
 
-// Assignment 3B
+
 // Please complete the ERC20 token with the following extensions;
 // 1) - Capped Token: The minting token should not be exceeded from the Capped limit.
 // 2) - TimeBound Token: The token will not be transferred until the given time exceed. For example Wages payment will be due after 30 days.
